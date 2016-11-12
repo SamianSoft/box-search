28ab3e5484d7285565230e279bbade84ed477fe4
alirni <aliramezani19.1@gmail.com>
2016-11-12 16:18:40 +0330

    chore(box-search.js): PE-9 fix tabs

--- box-search.js
+++ box-search.js
@@ -1,32 +1,32 @@
 (function (){
-  'use strict';
+	'use strict';
 
-  class BoxSearch extends Polymer.Element {
-    static get is () { return 'box-search'; }
+	class BoxSearch extends Polymer.Element {
+		static get is () { return 'box-search'; }
 
-    static get config () {
-      return {
-        properties: {},
-        observers: [],
-      }
-    }
+		static get config () {
+			return {
+				properties: {},
+				observers: [],
+			}
+		}
 
-	showClearButt (){
-		this.$.clearButt.style.opacity = "1";
-		this.$.clearButt.style.right = "5px";
+		showClearButt (){
+			this.$.clearButt.style.opacity = "1";
+			this.$.clearButt.style.right = "5px";
+		}
+
+		hideClearButt (){
+			this.$.clearButt.style.opacity = "0";
+			this.$.clearButt.style.right = "-50px";
+		}
+
+		clearInput (){
+			this.$.boxsearch.value = "";
+			this.$.clearButt.style.opacity = "0";
+
+		}
 	}
 
-	hideClearButt (){
-		this.$.clearButt.style.opacity = "0";
-		this.$.clearButt.style.right = "-50px";
-	}
-
-	clearInput (){
-		this.$.boxsearch.value = "";
-		this.$.clearButt.style.opacity = "0";
-
-	}
-  }
-
-  customElements.define(BoxSearch.is, BoxSearch);
+	customElements.define(BoxSearch.is, BoxSearch);
 })();
